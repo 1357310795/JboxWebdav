177e67876b7a589121cd1a15b0cc83d36eb63b19
1357310795 <1357310795@qq.com>
2022-07-24 09:16:54 +0800

    update consoleapp login

--- JboxWebdav.ConsoleApp/Program.cs
+++ JboxWebdav.ConsoleApp/Program.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Configuration;
-using System.Net;
-using System.Threading;
-using System.Xml;
+﻿using System.Net;
 using NWebDav.Server;
 using NWebDav.Server.Http;
 using NWebDav.Server.HttpListener;
 using NWebDav.Server.Logging;
 using NWebDav.Server.Stores;
-
 using NWebDav.Sample.HttpListener.LogAdapters;
 using System.Diagnostics;
 using Jbox.Service;
 using NWebDav.Server.Helpers;
+using JboxWebdav.ConsoleApp.Services;
+using System.Text;
 
 namespace NWebDav.Sample.HttpListener
 {
@@ -104,63 +101,124 @@ namespace NWebDav.Sample.HttpListener
 
         private static void Main(string[] args)
         {
-            Jac.ReadInfo();
-            if (Jac.dic.Count > 0)
+            Console.WriteLine("Welcome to JboxWebdav!");
+            var res = TryLogin();
+            if (!res)
             {
-                var ac = Jac.dic.Keys.First();
-                if (Jac.TryLastCookie(ac))
+                Console.WriteLine("Cookie登录失败，请使用账号密码登录");
+                while(true)
                 {
+                    Console.Write("Jaccount账号：");
+                    var account = Console.ReadLine();
+                    Console.Write("Jaccount密码：");
+                    var password = ReadPassword();
+                    var res1 = Jac.Login(account, password);
+                    if (res1.state == Jac.LoginState.success)
+                    {
+                        Console.WriteLine("登录成功！");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(res1.message);
+                    }
                 }
             }
-            // Use the Log4NET adapter for logging
             LoggerFactory.Factory = new ConsoleAdapter();
 
-
-            // Obtain the HTTP binding settings
-            var webdavProtocol =  "http";
-            //var webdavIp = "192.168.1.105";
-            //var webdavPort = "80";
+            var webdavProtocol = "http";
             var webdavIp = "127.0.0.1";
             var webdavPort = "65472";
 
             using (var httpListener = new System.Net.HttpListener())
             {
-                // Add the prefix
                 httpListener.Prefixes.Add($"{webdavProtocol}://{webdavIp}:{webdavPort}/");
 
-                // Use basic authentication if requested
-                var webdavUseAuthentication = false;
-                if (webdavUseAuthentication)
-                {
-                    // Check if HTTPS is enabled
-                    if (webdavProtocol != "https")
-                        Console.WriteLine("Most WebDAV clients cannot use authentication on a non-HTTPS connection");
-
-                    // Set the authentication scheme and realm
-                    httpListener.AuthenticationSchemes = AuthenticationSchemes.Basic;
-                    httpListener.Realm = "WebDAV server";
-                }
-                else
-                {
-                    // Allow anonymous access
-                    httpListener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
-                }
-
-                // Start the HTTP listener
+                httpListener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
                 httpListener.Start();
 
-                // Start dispatching requests
                 var cancellationTokenSource = new CancellationTokenSource();
                 DispatchHttpRequestsAsync1(httpListener, cancellationTokenSource.Token);
 
-                // Wait until somebody presses return
-                Console.WriteLine("WebDAV server running. Press 'x' to quit.");
+                Console.WriteLine("WebDAV 服务器运行中。按下 x 可以退出，按下 c 进入设置。");
                 while (Console.ReadKey().KeyChar != 'x') ;
                 Console.ReadKey();
                 cancellationTokenSource.Cancel();
             }
         }
 
-        
+        private static bool TryLogin()
+        {
+            Jac.InitStorage(new WinStorage());
+            Jac.ReadInfo();
+            if (Jac.dic.Count > 0)
+            {
+                var ac = Jac.dic.Keys.First();
+                if (Jac.TryLastCookie(ac))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static string ReadPassword()
+        {
+            //string res = "";
+            //while (true)
+            //{
+            //    ConsoleKeyInfo ck = Console.ReadKey(true);
+
+            //    if (ck.Key != ConsoleKey.Enter)
+            //    {
+            //        if (ck.Key != ConsoleKey.Backspace)
+            //        {
+            //            res += ck.KeyChar.ToString();
+            //            Console.Write("*");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("\b \b");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine();
+            //        break;
+            //    }
+            //}
+            //return res;
+            char cPassword;     //登陆时要用的密码 
+            StringBuilder cPass = new StringBuilder();//关键方法 
+            cPassword = Console.ReadKey(true).KeyChar;//输入字符可以让他不显示出来
+            while (cPassword != '\r')//回车
+            {
+                if (cPassword == '\b')//退格
+                {
+                    if (cPass.Length == 0)
+                    {
+                        cPassword = (char)Console.ReadKey(true).KeyChar;//输入字符可以让他不显示出来
+                    }
+                    else
+                    {
+                        cPass.Remove(cPass.Length - 1, 1);
+                        int cur_x = Console.CursorLeft;
+                        int cur_y = Console.CursorTop;
+                        Console.SetCursorPosition(cur_x - 1, cur_y);//光标定位    根据光标位置自己改动   x,y坐标
+                        Console.Write(" ");
+                        Console.SetCursorPosition(cur_x - 1, cur_y);//光标定位    根据光标位置自己改动
+                        cPassword = (char)Console.ReadKey(true).KeyChar;
+                    }
+                }
+                else
+                {
+                    cPass.Append(cPassword);
+                    Console.Write('*');
+                    cPassword = (char)Console.ReadKey(true).KeyChar;
+                }
+            }
+            Console.WriteLine();
+            return cPass.ToString();
+        }
     }
 }
